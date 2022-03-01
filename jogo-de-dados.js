f6292338e5dbd1ee696c390923893d5435a5e3d0
Zac Lima <jadielsonmanoel@gmail.com>
2022-03-01 01:34:53 -0300

    projeto finalizado

--- jogo-de-dados.js
+++ jogo-de-dados.js
@@ -1,39 +1,55 @@
 const prompt = require('prompt-sync')();
 
-let jogadores = []
-let numero = 0;
-
-const rodadas = +prompt("Rodadas: ");
-const qtdjogador = +prompt("Quantidades de Jogadores: ");
-for (let i = 1; i <= qtdjogador; i++){
-    let n = prompt(`Nome do ${i}° jogador: `)
-    let jog = { 'nome': n , 'valores':[],'pontos': 0}
-    jogadores.push(jog)
-    for (let j = 0; j < rodadas; j++){
-        numero = Math.floor(Math.random() * 6 + 1)
-        jog.valores.push(numero)
-    }
-    
+const qtdJogadores = +prompt('Quantidade de jogadores: ');
+const qtdRodadas = +prompt('Quantidade de rodadas: ');
+let jogadores = [];
+let rodada = [];
+let maiores = [];
+for (let i = 1; i <= qtdJogadores; i++){
+    const jogador = { nome: '', valores: [], pontos: 0 };
+    const jog = prompt(`Digite o nome do ${i}° jogador: `);
+    jogador.nome = jog;
+    jogadores.push(jogador);
 }
-let rodada = []
-for (let i = 0; i < jogadores.length; i++) {
-    for (let j = 0; j < jogadores.length; j++) {
-        let num = jogadores[j].valores[i]
-        rodada.push(num)
-    }
-    console.log(`Rodada: ${rodada}`)
-    for (let x = 0; x < rodada.length; x++) {
-        let maior = 0
-        if (rodada[x] > maior) {
-            maior = rodada[x]
-            for (let y = 0; y < jogadores.length; y++) {
-                if (maior == jogadores[y].valores[x]) {
-                    jogadores[y].pontos += 1
-                }
-            }
+for (let j = 0; j < qtdRodadas; j++) {
+    let maior = 0;
+    console.log('################');
+    console.log(`${j + 1}° rodada`);
+    console.log('################');
+    for (let i = 0; i < qtdJogadores; i++) {
+        let dadoJogado = parseInt(Math.floor(Math.random() * 6 + 1));
+        console.log(`${jogadores[i].nome} tirou ${dadoJogado}`);
+        jogadores[i].valores.push(dadoJogado);
+        rodada.push(dadoJogado);
+        if (dadoJogado > maior) {
+            maior = dadoJogado;
         }
-        rodada = []
+        if (i == qtdJogadores - 1) {
+            maiores.push(maior);
+        }
     }
 }
-
-console.log(jogadores)
+console.log('\n');
+for (let j = 0; j < qtdRodadas; j++) {
+    for (let i = 0; i < qtdJogadores; i++) {
+        if (maiores[j] == jogadores[i].valores[j]) {
+            jogadores[i].pontos += 1;
+        }
+    }
+}
+let maiorPontuacao = 0;
+let vencedor = [];
+for (let i = 0; i < qtdJogadores; i++) { 
+    if (maiorPontuacao < jogadores[i].pontos) {
+        maiorPontuacao = jogadores[i].pontos;
+    }
+}
+console.log('################')
+for (let i = 0; i < qtdJogadores; i++) { 
+    console.log(`${jogadores[i].nome} fez ${jogadores[i].pontos} pontos.`);
+    if (maiorPontuacao == jogadores[i].pontos) {
+        vencedor.push(jogadores[i].nome);
+    }
+}
+console.log('################');
+console.log(`\nO grande vencedor foi ${vencedor}!`);
